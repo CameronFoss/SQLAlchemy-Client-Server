55b637f1ac01b5cd30008d67c09b380b0dd297ec
fossc <fossc@umich.edu>
2021-05-25 14:53:22 -0400

    Expanded celery demo to be more interactive. The celery producer can now read from any table with input from the user, much like the client in the main project, but a little simpler.

--- training/celery_demo/celery.py
+++ training/celery_demo/celery.py
@@ -4,4 +4,4 @@ from celery import Celery
 app = Celery('celery_demo',
              broker='amqp://cameron:foxkilla@localhost/cameron_vhost',
              backend='rpc://',
-             include=['celery_demo.tasks', 'training.server.db_utils', 'training', 'training.server', 'training.model'])
+             include=['celery_demo.tasks', 'training.server.db_utils', 'training', 'training.server', 'training.model', 'training.client.choice_funcs'])
